a2e8e22e62e4883f58010744483815b4342d97ff
Florian <floriansense@gmail.com>
2016-04-04 17:04:04 +0200

    Changes based on reviewer comments
    The paper was accepted with minor revisions. Those were implemented
    here.

--- figures/effects.R
+++ figures/effects.R
@@ -16,47 +16,47 @@ effDF$first = factor(which.first[effDF$sub])
 pdf(file = "effects.pdf", width = 10, height = 5, version = "1.4")
 par(mfrow = c(1,2), las=1, bty="n")
 
-############### 
+###############
 ##### Figure 3A:
-plot(effDF[effDF$seq == "sequential","eff"], effDF[effDF$seq == "simultaneous","eff"], 
-     ylim = c(-.25,.25), xlim = c(-.25,.25), ylab = "Simultaneous silent advantage", 
+plot(effDF[effDF$seq == "sequential","eff"], effDF[effDF$seq == "simultaneous","eff"],
+     ylim = c(-.25,.25), xlim = c(-.25,.25), ylab = "Simultaneous silent advantage",
      xlab = "Sequential silent advantage", pch = as.character(effDF[effDF$seq == "sequential","ss"]),
      col = 1)#as.integer(effDF$first))
 
-segments(effDF[effDF$seq == "sequential","eff"] - effDF[effDF$seq == "sequential","stdErr"], 
-       effDF[effDF$seq == "simultaneous","eff"], 
-       effDF[effDF$seq == "sequential","eff"] + effDF[effDF$seq == "sequential","stdErr"], 
+segments(effDF[effDF$seq == "sequential","eff"] - effDF[effDF$seq == "sequential","stdErr"],
+       effDF[effDF$seq == "simultaneous","eff"],
+       effDF[effDF$seq == "sequential","eff"] + effDF[effDF$seq == "sequential","stdErr"],
        effDF[effDF$seq == "simultaneous","eff"],
        col="lightgray")
 
 segments(effDF[effDF$seq == "sequential","eff"],
-         effDF[effDF$seq == "simultaneous","eff"] - effDF[effDF$seq == "simultaneous","stdErr"], 
-         effDF[effDF$seq == "sequential","eff"], 
-         effDF[effDF$seq == "simultaneous","eff"] + effDF[effDF$seq == "simultaneous","stdErr"], 
+         effDF[effDF$seq == "simultaneous","eff"] - effDF[effDF$seq == "simultaneous","stdErr"],
+         effDF[effDF$seq == "sequential","eff"],
+         effDF[effDF$seq == "simultaneous","eff"] + effDF[effDF$seq == "simultaneous","stdErr"],
          col="lightgray")
 
-points(effDF[effDF$seq == "sequential","eff"], effDF[effDF$seq == "simultaneous","eff"], 
+points(effDF[effDF$seq == "sequential","eff"], effDF[effDF$seq == "simultaneous","eff"],
  pch = as.character(effDF[effDF$seq == "sequential","ss"]),
  col = 1)#as.integer(effDF$first))
 abline(0,1, lty=2)
-text(-.24,.24,adj=0,"Simultaneous advantage (28/45)")
-text(.24,-.24,adj=1,"Sequential advantage (17/45)")
+text(-.24,.24,adj=0,"Simultaneous advantage (28/45)", col="darkgray")
+text(.24,-.24,adj=1,"Sequential advantage (17/45)", col="darkgray")
 
 sum((effDF[effDF$seq == "sequential","eff"] - effDF[effDF$seq == "simultaneous","eff"]) > 0)
 nrow(effDF[effDF$seq == "sequential",])
 
-############### 
+###############
 ##### Figure 3B:
 corr.by.block = with(data,tapply(CResp,list(blocknum,setSize,silent,sequential),mean))
 adv.for.silent = corr.by.block[,,"silent",] - corr.by.block[,,"articulate",]
 effect.by.pres = adv.for.silent[,,"sequential"] - adv.for.silent[,,"simultaneous"]
 
-matplot(effect.by.pres, ylab="Effect (seq - sim)", 
+matplot(effect.by.pres, ylab="Effect (seq - sim)",
         xlab="Block (2 per session)",ty='b',pch=c("2","4","8"),ylim=c(-.21,.21), col=1)
 abline(h=0,col="gray")
 # text(10,.2,"Silent adv. bigger for seq. pres.",adj=1, col="black")
 # text(10,-.2,"Silent adv. bigger for sim. pres.",adj=1, col="black")
-text(10,.2,"Silent adv. bigger for sequential pres.",adj=1, col="gray")
-text(10,-.2,"Silent adv. bigger for simultaneous pres.",adj=1, col="gray")
+text(10,.2,"Silent adv. bigger for sequential pres.",adj=1, col="darkgray")
+text(10,-.2,"Silent adv. bigger for simultaneous pres.",adj=1, col="darkgray")
 
 dev.off()
